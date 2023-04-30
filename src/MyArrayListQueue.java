999d1fef8c06de5b77ed3ba608ae463d2aef77cf
dariyasekerbek <Dariya.Sekerbek@kz.ey.com>
2023-04-30 14:32:18 +0600

    peek, isEmpty and size methods

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -17,7 +17,19 @@ public class MyArrayListQueue<T> {
         }
         return arr_list.remove(0);
     }
-
-
-
+    // method that return element at the beginnig without removing
+    public T peek(){
+        if(arr_list.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return arr_list.get(0);
+    }
+    // check if queue is empty or not
+    public boolean isEmpty(){
+        return arr_list.isEmpty();
+    }
+    // return the size of the queue
+   public int size(){
+        return arr_list.size();
+   }
 }
