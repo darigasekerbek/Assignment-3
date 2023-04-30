7ed48900229cdba926d70a4ef686274b24d12f77
dariyasekerbek <Dariya.Sekerbek@kz.ey.com>
2023-04-30 14:13:01 +0600

    Test for MyArrayListStack

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main{
         arr_stack.push(111);
         arr_stack.push(222);
         arr_stack.push(333);
+        System.out.println("Size of stack: "+ arr_stack.size());
+        System.out.println("Top element of stack:" + arr_stack.peek());
+        while(!arr_stack.isEmpty()){
+            System.out.println(arr_stack.pop());
+        }
 
     }
 }
