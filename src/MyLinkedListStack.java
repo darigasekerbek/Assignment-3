e9a3ec55ba70402f1b14e40d73f942a7a5000632
dariyasekerbek <Dariya.Sekerbek@kz.ey.com>
2023-04-30 13:46:43 +0600

    MyLinkedListStack<T>

---
+++ src/MyLinkedListStack.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+import java.util.EmptyStackException;
+public class MyLinkedListStack<T> {
+    private LinkedList<T> link_list;
+    public MyLinkedListStack(){
+        link_list = new LinkedList<>();
+    }
+}
