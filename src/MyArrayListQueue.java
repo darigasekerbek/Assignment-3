985a9c5454b6a367dacecca668020d9614bd00e2
dariyasekerbek <Dariya.Sekerbek@kz.ey.com>
2023-04-30 14:28:23 +0600

    enqueue(T item) T dequeue()

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,12 +1,21 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class MyArrayListQueue<T> {
     private ArrayList<T> arr_list;
     public MyArrayListQueue(){ // constructor that init ArrayList
         arr_list = new ArrayList<>();
     }
+    //add element to the end of queue
     public void enqueue(T item){
         arr_list.add(item);
+    }
+    //remove and return element at the beginning of queue
+    public T dequeue(){
+        if (arr_list.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return arr_list.remove(0);
     }
 
 
