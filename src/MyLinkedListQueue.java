b4c603a57d83df43ff9a2aabe0db6d8a1425bddd
dariyasekerbek <Dariya.Sekerbek@kz.ey.com>
2023-04-30 14:46:29 +0600

    dequeue and peek methods

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 public class MyLinkedListQueue<T> {
     private LinkedList<T> link_list;
@@ -8,5 +9,17 @@ public class MyLinkedListQueue<T> {
     public void enqueue(T item){ // add item to the back of queue
         link_list.addLast(item);
     }
+   public T dequeue(){ //remove element at the front of the queue and return it
+        if (link_list.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return link_list.removeFirst();
+   }
+   public T peek(){ // like dequeue but, without removing element
+       if (link_list.isEmpty()){
+           throw new NoSuchElementException();
+       }
+       return link_list.getFirst();
+   }
 
 }
