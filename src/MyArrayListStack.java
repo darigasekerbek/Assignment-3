ab8c15c7586947bfb6a9996c93883dee0e9ae3c0
dariyasekerbek <Dariya.Sekerbek@kz.ey.com>
2023-04-30 13:11:27 +0600

    MyArrayListStack

---
+++ src/MyArrayListStack.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class MyArrayListStack<T> {
+    private ArrayList<T> arr_list;
+    public MyArrayListStack(){
+        arr_list = new ArrayList<>();
+    }
+
+}
