19070d98cce4298641b704d69a71720f9b2b9b23
dariyasekerbek <Dariya.Sekerbek@kz.ey.com>
2023-04-30 14:18:13 +0600

    Arr and Linked

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Main{
     public static void main(String[] args) {
         MyArrayListStack<Integer> arr_stack = new MyArrayListStack<>();
+        System.out.println("MyArrayListStack");
         arr_stack.push(111);
         arr_stack.push(222);
         arr_stack.push(333);
@@ -11,7 +12,10 @@ public class Main{
         while(!arr_stack.isEmpty()){
             System.out.println(arr_stack.pop());
         }
+
+
         MyLinkedListStack<Integer> link_stack = new MyLinkedListStack<>();
+        System.out.println("MyLinkedListStack");
         link_stack.push(101);
         link_stack.push(102);
         link_stack.push(103);
