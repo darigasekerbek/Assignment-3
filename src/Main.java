379ec209f5b43334913acdd8a967e226daf6210b
dariyasekerbek <Dariya.Sekerbek@kz.ey.com>
2023-04-30 13:34:50 +0600

    T peek()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class Main{
     public static void main(String[] args) {
+        MyArrayListStack<Integer> arr_stack = new MyArrayListStack<>();
+        arr_stack.push(111);
+        arr_stack.push(222);
+        arr_stack.push(333);
 
     }
 }
--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -17,5 +17,12 @@ public class MyArrayListStack<T> {
         }
         return arr_list.remove(arr_list.size()-1);
     }
+    // return item on the top without removing
+    public T peek(){
+        if(arr_list.isEmpty()){
+            throw new EmptyStackException();
+        }
+        return arr_list.get(arr_list.size()-1);
+    }
 
 }
